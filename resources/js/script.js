9ac7a3b453941ed8dda2275f476ed627f31e79ac
Saksham Singh <geekysaksham27@gmail.com>
2019-10-16 03:12:22 +0530

    added some scroll animations

--- resources/js/script.js
+++ resources/js/script.js
@@ -21,10 +21,14 @@ $(document).ready(function () {
         $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000)
     });
 
+
+
     /* ============== Scroll on Logo ============== */
     $('.js--scroll-to-home').click(function() {
         $('html, body').animate({scrollTop :$('.js--home').offset().top }, 1000)
     });
+
+
 
     /* ======== Scroll on Navigation ============ */
     $('a[href*="#"]').on('click', function (e) {
@@ -38,6 +42,33 @@ $(document).ready(function () {
             'linear'
         )
     })
+
+
+
+    /* =========== Animations on scroll =========== */
+    $('.js--wp-1').waypoint(function() {
+        $('.js--wp-1').addClass('animated fadeIn slow');
+    },{
+        offset: '70%'
+    });
+
+    $('.js--wp-2').waypoint(function() {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    },{
+        offset: '40%'
+    });
+
+    $('.js--wp-3').waypoint(function () {
+        $('.js--wp-3').addClass('animated fadeIn slow');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-4').waypoint(function () {
+        $('.js--wp-4').addClass('animated pulse short');
+    }, {
+        offset: '25%'
+    });
 
 }
 
